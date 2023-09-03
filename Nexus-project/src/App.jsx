6aa25e000cb379da841c6dacfd07fb8c2378a9d2
Prasannathondamalla06 <dorathondamalla06@gmail.com>
2023-09-03 15:29:28 +0530

    feat: :zap: second commit

--- Nexus-project/src/App.jsx
+++ Nexus-project/src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
 import Home from  "./Component/Home";
 import Header from "./Component/Header" ;
-import './App.css'
+import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
  
 
   return (
     <>
-    <Header />
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+   
       
    </>
   )
